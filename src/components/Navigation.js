ce77d600f6747fa6b77078754efcd6ce0c69d660
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-06 11:12:40 -0400

    add footer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,15 +7,30 @@ import './styles/nav.scss';
 
 function Navigation() {
   return (
+    <>
     <Navbar bg="dark" variant="dark">
       <Container fluid>
-      <Navbar.Brand href="#home">AutoLib</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#home">About</Nav.Link>
-        <Nav.Link href="#features">Grab A Book</Nav.Link>
-      </Nav>
+        <Navbar.Brand href="#home">AutoLib</Navbar.Brand>
+
+        <Nav className="me-auto">
+          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#features">Grab A Book</Nav.Link>
+        </Nav>
+
+        <Nav>
+          <Nav.Link href="#login">Sign In</Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
+
+    <div className="fixed-bottom">  
+      <Navbar bg="dark" variant="dark">
+          <Container fluid>
+              <Navbar.Brand>Footer</Navbar.Brand>
+          </Container>
+      </Navbar>
+    </div>
+    </>
   );
 }
 
