245ceeaf00c91fce0e9de50f0047e5ff5753e13c
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-06 10:42:25 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container fluid>
-        <Navbar.Brand href="#home">AutoLib</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#features">Grab A Book</Nav.Link>
-        </Nav>
-        </Container>
-      </Navbar>
       <Navigation />
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './styles/nav.scss';
 
 function Navigation() {
   return (
-    <h2 className="nav">App</h2>
+    <Navbar bg="dark" variant="dark">
+      <Container fluid>
+      <Navbar.Brand href="#home">AutoLib</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">About</Nav.Link>
+        <Nav.Link href="#features">Grab A Book</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
