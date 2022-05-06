9947c685940c8247d833ce094918c9581dc9969b
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-06 11:28:37 -0400

    install react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaFacebook} from 'react-icons/fa'
 
 function Footer() {
   return (
     <footer className="fixed-bottom">
       <Navbar bg="dark" variant="dark">
         <Container fluid>
-            <Navbar.Brand>Footer</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#contact">Contact Us</Nav.Link>
+          </Nav>
+
+          <Nav>
+            <Nav.Link href="#connect">Connect With Us</Nav.Link>
+            <Nav.Link href="#connect"><FaFacebook /></Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     </footer>
