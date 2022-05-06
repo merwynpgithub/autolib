a727d285858005e5972d823c7d36f6b7575401e0
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-06 10:40:41 -0400

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-// import './App.css';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <h1>AutoLib</h1>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+        <Navbar.Brand href="#home">AutoLib</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#features">Grab A Book</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
       <Navigation />
     </div>
   );
