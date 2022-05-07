1e1fe6f76dde212da19baa688794a7be8120fb75
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-07 16:24:40 -0400

    update About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -18,7 +18,7 @@ function About() {
         }
       }>About AutoLib:</p>
       <p>AutoLib is a local community library founded with the purpose of serving local community members in an automated way without relying on a storage space and a central library building. </p>
-      <p>Autolib aims at combining the concept of peer to peer sharing with a traditional functionality of a library</p>
+      <p>AutoLib aims at combining the concept of peer to peer sharing with a traditional functionality of a library.</p>
 
     </div>
   );
