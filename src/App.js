8d4eeeed35040ab41f5d77334b8e3f584137ef8b
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-08 15:51:14 -0400

    add new book route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Books from './components/pages/Books';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
 import Book from './components/pages/Book';
+import NewBook from './components/pages/NewBook';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/new" element={<NewBook />}/>
           <Route path="/about" element={<About />}/>
           <Route path="/books/*" element={<Books />}/>
           <Route path="/books/:bookId" element={<Book />}/>
---
+++ src/components/pages/NewBook.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function NewBook() {
+  return (
+    <div style={
+      {
+        width: "40%",
+        margin: "auto",
+        minWidth: "350px",
+        marginTop: "3em"
+      }
+    }
+    >
+      <h2>New Book Form</h2>
+    </div>
+  );
+}
+
+export default NewBook;
