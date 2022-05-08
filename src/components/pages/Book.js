e876b9f995a1423442f2a13e655578df47de647c
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-08 13:24:50 -0400

    render book details

--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -1,11 +1,60 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 
 function Book() {
+  const [bookDetails, setBookDetails] = useState({});
+
   const parameter = useParams();
-  console.log(parameter);
+  const bookId = Number(parameter.bookId);
+
+  useEffect(() => {
+    axios.get("/api/resources").then(res => {
+      const bookArray = res.data;
+      const renderedBook = bookArray.filter(book => book.id === bookId);
+      console.log(renderedBook);
+      setBookDetails(renderedBook[0]);
+    });
+  }, [])
   return (
-    <p>This is book {parameter.bookId}</p>
+    <>
+      <p>This is book {
+        parameter.bookId
+      }</p>
+      <div style={
+        {
+          width: "75%",
+          margin: "auto",
+          minWidth: "350px",
+          marginTop: "3em",
+          display: "flex",
+          justifyContent: "space-around"
+        }
+      }>
+        <div>
+          <img src={
+              bookDetails.cover_image
+            }
+            alt={
+              bookDetails.title
+            }
+            style={
+              {height: "350px"}
+            }/>
+        </div>
+
+        <div style={{marginLeft: "2em"}}> 
+          <p style={{fontSize: "1.5em"}}>{bookDetails.title}</p>
+          <p style={{fontSize: "1.2em"}}>ISBN: {bookDetails.isbn}</p>
+          <p>{bookDetails.description}</p>
+          <p>Authors: {bookDetails.authors}</p>
+          <p>Genres: {bookDetails.genres}</p>
+          <p>Status: {bookDetails.status}</p>
+        </div>
+
+      </div>
+
+    </>
   );
 }
 
