cf6143f34d8c88b4474036b22318e82905b53b5a
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-08 16:02:10 -0400

    add form for new book

--- src/components/pages/NewBook.js
+++ src/components/pages/NewBook.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Form, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NewBook() {
@@ -13,6 +14,31 @@ function NewBook() {
     }
     >
       <h2>New Book Form</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="isbn">
+          <Form.Label>ISBN</Form.Label>
+          <Form.Control type="text" placeholder="Enter ISBN"/>
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" placeholder="Enter Title"/>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="author">
+          <Form.Label>Author</Form.Label>
+          <Form.Control type="text" placeholder="Enter Author"/>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
