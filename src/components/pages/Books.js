0586b03cb725c14eb15843be169b115596bfdac2
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-08 12:10:18 -0400

    render book details using axios

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,12 +1,34 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Books() {
   const [book, setBook] = useState([]);
-  
+
   useEffect(() => {
-    axios.get("/api/resources").then(res=> setBook(res.data));
+    axios.get("/api/resources").then(res => setBook(res.data));
   }, [])
+
+  const bookList = book.map(book => {
+    return (
+      <div key={
+          book.id
+        }
+        style={
+          {margin: "10px"}
+      }>
+        <img src={
+            book.cover_image
+          }
+          alt={
+            book.title
+          }
+          style={
+            {height: "250px"}
+          }/>
+          <p>{book.title}</p>
+      </div>
+    );
+  });
 
   return (
     <>
@@ -25,13 +47,16 @@ function Books() {
             textAlign: "center"
           }
         }>Grab a Book</p>
-        <div>
-          <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg" alt="lord of the rings"
-            style={
-              {height: "250px"}
-            }/>
+        <div style={
+          {
+            width: "75%",
+            aspectRatio: "2/3",
+            margin: "auto",
+            display: "flex"
+          }
+        }>
+          {bookList} 
         </div>
-        
       </div>
     </>
   );
