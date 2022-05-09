03c522e3c0358d7b44e93d1344fd5178535cfc34
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-09 11:23:43 -0400

    add logout in navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Dropdown, DropdownButton } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './styles/nav.scss';
 
 function Navigation() {
-  const islogged = false;
+  const [islogged, setLogged] = useState();
+  
+  function handleClick(e) {
+    setLogged(false);
+  }
   return (
     <>
     <Navbar bg="dark" variant="dark">
@@ -21,7 +25,9 @@ function Navigation() {
           <Nav>
             <Nav.Link href="/new">Add Book</Nav.Link>
             {!islogged && <Nav.Link href="/login">Sign In</Nav.Link>}
-            {islogged && <Nav.Link href="/user">Carolin</Nav.Link>}
+            {islogged && <DropdownButton id="dropdown-basic-button" title="Carolin">
+              <Dropdown.Item href="/" onClick={handleClick}>Logout</Dropdown.Item>
+            </DropdownButton>}
           </Nav>
         </Container>
 
