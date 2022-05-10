74a3ee2ecbc80ba1ee8d487050338eaaed9a4dac
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-05-09 21:42:58 -0400

    remove typo in book scss and update homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Navigation from '../Navigation';
+import '../styles/books.scss';
 
 function Home() {
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/resources").then(res => setBook(res.data));
+  }, [])
+
+  const bookList = book.map(book => {
+    return (
+      <div className="book-list" key={book.id}>
+        <img src={book.cover_image} alt={book.title} />
+        <p>{book.title}</p>
+      </div>
+    );
+  });
+
   return (
     <>
     <Navigation />
-    <div style={
-      {
-        width: "75%",
-        margin: "auto",
-        minWidth: "350px",
-        marginTop: "3em"
-      }
-    }>
-      <p style={
-        {
-          fontWeight: "bold",
-          fontSize: "1.5em",
-          textAlign: "center"
-        }
-      }>Recently Added Books</p>
-
+    <div className="container-fluid book-display" >
+      <p id="grab">Recently Added Books</p>
+      <div className="container-fluid books" >{bookList} </div>
     </div>
     </>
     
