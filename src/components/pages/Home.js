44d4d5013a27f0562fabc8a4a08b46599b3a5053
Johannes Weber <johannes@drweber.de>
2022-05-18 17:24:47 -0700

    add key to SwiperSlide elements (unique key warning)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,21 +15,18 @@ import { EffectCards } from "swiper";
 
 
 function Home() {
-  const [book, setBook] = useState([]);
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/resources/random?limit=7").then(res => setBook(res.data));
+    axios.get("/api/resources/random?limit=7").then(res => setBooks(res.data));
   }, [])
 
-  const bookList = book.map(book => {
+  const bookList = books.map(book => {
     return (
-        <SwiperSlide><img src={book.cover_image} alt={book.title} /></SwiperSlide>
-     
-      // <div className="book-list" key={book.id}>
-      //   <img src={book.cover_image} alt={book.title} />
-      // </div>
+      <SwiperSlide key={book.id}><img src={book.cover_image} alt={book.title}/></SwiperSlide>
     );
   });
+
 
   return (
     <>
@@ -45,24 +42,24 @@ function Home() {
           effect={"cards"}
           modules={[EffectCards]}
           className="mySwiper section_2"
-          key={book.id}
-        >{bookList} </Swiper>
+        >{bookList}
+        </Swiper>
     </div>
     <main>
       <p className='header'>HOW IT WORKS</p>
       <div className='wrapper'>
         <div className='container'>
-          <div className='white_back'><img src="/search.png" /></div>
+          <div className='white_back'><img src="/search.png" alt="search" /></div>
           <p className='subheader'>Find A Book</p>
           <p>Look through the endless collection of books in the library, and select one you’re excited to read</p>
         </div>
         <div className='container'>
-          <div className='white_back'><img src="/transaction.png" /></div>
+          <div className='white_back'><img src="/transaction.png" alt="transaction"/></div>
           <p className='subheader'>Make The Request</p>
           <p>Request the book from the current possessor, and wait for the your request to be accepted</p>
         </div>
         <div className='container'>
-          <div className='white_back'><img src="/open-book.png" /></div>
+          <div className='white_back'><img src="/open-book.png" alt="open-book"/></div>
           <p className='subheader'>Pick It Up</p>
           <p>Make arrangements for pick up at an arranged and convenient location. Share the love by uploading a book or two!</p>
         </div>
