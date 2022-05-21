7a3b85927c1513374a97162d77eb34c16d814498
Johannes Weber <johannes@drweber.de>
2022-05-21 11:25:34 -0700

    add filtering option, expand availability indicators

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Form, Button, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,14 +11,20 @@ import '../styles/grab.scss';
 function Books() {
   const [book, setBook] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [filtered, setFiltered] = useState(false);
+  const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem("user"));
 
   function handleSearch(e) {
     e.preventDefault();
-    const url = "/api/resources?find=" + searchValue;
+    const url = "/api/resources?withStatus&find=" + searchValue;
     
     axios.get(url)
     .then(res => {
-      setBook(res.data);
+      const books = [];
+      res.data.forEach(record => books.push({...record, status: { ...record.status }}));
+      setBook(books);
     });
   }
   
@@ -25,21 +32,59 @@ function Books() {
     
     axios.get("/api/resources?withStatus").then(res => {
       const bookCheck = res.data.map(book => {
-        if (book.status.available) return {...book, canGrab: true };
+        const bookRecord = { ...book, status: { ...book.status} };
+        if (book.status.available) return {...bookRecord, canGrab: true };
         else return {...book, canGrab: false };
       });
       setBook(bookCheck);
     });
   }, [])
   
-  const defaultImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqlyVgjqhw65UtsRfTi-zafiSFb7zitbpQAjcrlqKZcCgBiDyWv4MV4CbgVcxFlXtf-8I&usqp=CAU";
+  const defaultImageUrl = "/no-photo-available.png";
   
-  const bookList = book.map(book => {
+  const requestBook = book => {
+    axios.post("/api/requests", { "resourceId": book.id })
+    .then(() => {
+      navigate("/request");
+    })
+    .catch(err => console.log(err));
+  }
+
+  const getRequestLink = book => {
+    let s = '';
+    if (book.status?.available && book.current_possessor_id!==user.id) {
+      s = <p onClick={() => requestBook(book)}className="request">Request now</p>
+    } 
+    return s;
+  }
+  
+  const getAvailability = book => {
+    let s = '';
+    if (!book.status?.available) {
+      if (book.current_possessor_id!==user.id) {
+        if (book.status?.availableAt) {
+          s = <p className="unavailable">Available {new Date(book.status.availableAt).toLocaleDateString()}</p>;
+        } else {
+          s = <p className="unavailable">Available soon</p>;
+        }
+      } else {
+        s = <p className="in-my-possession">Yours until {new Date(book.status.availableAt).toLocaleDateString()}</p>;
+      }
+    } else {
+      if (book.current_possessor_id===user.id) {
+        s = <p className="in-my-possession">You have this book</p>;
+      }
+    }
+    return s;
+  }
+
+  const bookList = book.filter(b => !filtered || b.status?.available).map(book => {
     return (
       <div className="book" key={book.id}>
         <a href={"/books/" + book.id}><img src={book.cover_image || defaultImageUrl} alt={book.title} loading="lazy" /></a>
         <p className='title'>{book.title}</p>
-        {book.canGrab === false && <p className="unavailable">UNAVAILABLE</p>}
+        {getAvailability(book)}
+        {getRequestLink(book)}
       </div>
     );
   });
@@ -52,7 +97,15 @@ function Books() {
           <h1 id="grab">CATALOGUE</h1>
           <div className="search">
             <Form className="d-flex" onSubmit={handleSearch}>
-              <FormControl
+            <Form.Check 
+              className="filter"
+              type={'checkbox'}
+              id={`default-checkbox`}
+              label={`Hide unavailable books`}
+              checked={filtered}
+              onChange={() => setFiltered(!filtered)}
+            />
+            <FormControl
                 type="search"
                 placeholder="Search Title, Author, Genre"
                 className="me-2"
