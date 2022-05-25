dc476b971b72900df124648dc4b9b1f78adbf8dc
Johannes Weber <johannes@drweber.de>
2022-05-24 21:42:49 -0700

    use getAvailability() helper for consistent status info

--- src/components/pages/UserBookThumb.js
+++ src/components/pages/UserBookThumb.js
@@ -1,17 +1,15 @@
 import React from "react";
-
+import { getAvailability } from "../../helper/book-utilities";
 const UserBookThumb = props => {
   const { book } = props;
 
-  const availability = book.status?.availableAt
-  ? <span className='text-success'>expires {new Date(book.status?.availableAt).toLocaleDateString()}</span>
-  : <span className='text-warning'>expired/in library</span>
+  const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : undefined;
 
   return(
     <div className="book" key={book.id}>
       <a href={"/books/" + book.id}><img src={book.cover_image || '/no-photo-available.png'} alt={book.title} loading="lazy" /></a>
       <p className='title'>{book.title}</p>
-      <p>{availability}</p>
+      {getAvailability(book, user, true)}
     </div>
   )
 };
