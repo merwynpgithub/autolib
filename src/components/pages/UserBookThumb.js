9baa5664b52af3ff2bc2beb0cb4740b22f96312d
Johannes Weber <johannes@drweber.de>
2022-05-29 15:58:52 -0700

    use useNavigate instead of <a>

--- src/components/pages/UserBookThumb.js
+++ src/components/pages/UserBookThumb.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, Link, useNavigate } from "react-router-dom";
 import { getAvailability } from "../../helper/book-utilities";
 const UserBookThumb = props => {
   const appData = useOutletContext();
+  const navigate = useNavigate();
+
   const { book } = props;
+
+  //This is a workaround. For some reason using the <Link> component instead of <a> causes errors.
+  const navToBookPage = (e) => {
+    e.preventDefault();
+    navigate(`/books/${book.id}`);
+  }
 
   return(
     <div className="book" key={book.id}>
-      <a href={"/books/" + book.id}><img src={book.cover_image || '/no-photo-available.png'} alt={book.title} loading="lazy" /></a>
+      <a href={"/books/" + book.id} onClick={navToBookPage}><img src={book.cover_image || '/no-photo-available.png'} alt={book.title} loading="lazy" /></a> 
       <p className='title'>{book.title}</p>
       {getAvailability(book, appData.user, true)}
     </div>
