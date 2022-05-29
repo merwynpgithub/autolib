fc68ce59fdfcfc0f72b3d95259d441d01f58a82d
Johannes Weber <johannes@drweber.de>
2022-05-28 21:00:41 -0700

    use useOutletContext

--- src/components/pages/UserBookThumb.js
+++ src/components/pages/UserBookThumb.js
@@ -1,15 +1,15 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 import { getAvailability } from "../../helper/book-utilities";
 const UserBookThumb = props => {
+  const appData = useOutletContext();
   const { book } = props;
-
-  const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : undefined;
 
   return(
     <div className="book" key={book.id}>
       <a href={"/books/" + book.id}><img src={book.cover_image || '/no-photo-available.png'} alt={book.title} loading="lazy" /></a>
       <p className='title'>{book.title}</p>
-      {getAvailability(book, user, true)}
+      {getAvailability(book, appData.user, true)}
     </div>
   )
 };
