b42a0061bfe6a6889adaedb48357c441f8d1f034
Johannes Weber <johannes@drweber.de>
2022-05-29 21:35:20 -0700

    remove obsolete state

--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -13,14 +13,9 @@ import { getAvailability } from '../../helper/book-utilities';
 
 function Book() {
   const appData = useOutletContext();
-  let navigate = useNavigate();
-  const [bookDetails, setBookDetails] = useState({});
-  const [status, setStatus] = useState({});
-  const [hasBook, setHasBook] = useState(false);
-  const [mapUrl, setMapUrl] = useState("");
+  const navigate = useNavigate();
 
-  // For Grab button
-  let notLogged = !appData?.user?.id;
+  const [bookDetails, setBookDetails] = useState({});
  
   const parameter = useParams();
   const bookId = Number(parameter.bookId);
@@ -33,86 +28,78 @@ function Book() {
     })
     .catch(err => console.log(err));
   }
+
   useEffect(() => {
     //Load the single Book
     const url = "/api/resources/" + bookId;
     axios.get(url).then(res => {
       const record = res.data;
-      const newBookDetails = { ...record, status: { ...record.status }};
-      setBookDetails(newBookDetails);
-      setStatus(res.data.status);
-
-      //Check if user has that book
-      if (appData.user.id) {
-        const url = "/api/resources?current_possessor_id=" + appData.user.id;
-        axios.get(url).then(data => {
-          const booksPossessed = data.data;
-          const currentBook = res.data;
-          //Update if user has book
-          const doesHaveBook = booksPossessed.filter(book => book.id === currentBook.id);
-          setHasBook(doesHaveBook.length >= 1);
-          //Show the book location in a map
-          const urlSrc = addressConverter(res.data);
-          setMapUrl(urlSrc);
-        })
-
-      }
-      
+      const newBookDetails = { 
+        ...record, 
+        status: { ...record.status },
+        userHasBook: record.current_possessor_id === appData.user?.id,
+        mapUrl: addressConverter(record),
+      };
+      setBookDetails(newBookDetails);      
     });
   }, [bookId, appData.user])
 
-  const defaultImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqlyVgjqhw65UtsRfTi-zafiSFb7zitbpQAjcrlqKZcCgBiDyWv4MV4CbgVcxFlXtf-8I&usqp=CAU";
+  const defaultImageUrl = "/no-photo-available.png";
 
-  return (
-    <>
-      <Navigation appData={appData}/>
-      <div className='book_wrapper'>
-        <div className='section_1'>
-          <img 
-            src={bookDetails.cover_image || defaultImageUrl}
-            alt={bookDetails.title}
-          />
-        </div>
+  if (bookDetails.id) {
 
-        <div className='section_2'>
-          <p id="title">{bookDetails.title}</p>
-          <div className='inner'>
-            <div className='inner_1'>
-              <p className='bold author'>by {bookDetails.authors}</p>
-              <br />
-              <p><span className="bold">Genre(s):</span> {bookDetails.genres}</p>
-              <p><span className="bold">ISBN:</span> {bookDetails.isbn}</p>
-              
-              <br />
-              <p>{bookDetails.description}</p>
-              <div className="status-info">{getAvailability(bookDetails, appData.user)}</div>
-            </div>
-            <div className='inner_2'>
-              {status.text === "available" && notLogged && 
-                <Form>
-                  <Button variant="primary" type="submit" disabled>
-                    Sign In to Request
-                  </Button>
-                </Form>}
-                {status.text === "available" && appData.user && !hasBook &&
-                <Form onSubmit={handleSubmit}>
-                  <Button className='button' type="submit">
-                    Get This Book
-                  </Button>
-                </Form>}
-            </div>
+    return (
+      <>
+        <Navigation appData={appData}/>
+        <div className='book_wrapper'>
+          <div className='section_1'>
+            <img 
+              src={bookDetails.cover_image || defaultImageUrl}
+              alt={bookDetails.title}
+            />
           </div>
-          {appData.user && !hasBook && <div>
-            <p className="bold">Book Location:</p>
-            <br />
-            <iframe style={{width: "100%", height: "300px"}} id="gmap_canvas" src={mapUrl}></iframe>
-          </div>}
+  
+          <div className='section_2'>
+            <p id="title">{bookDetails.title}</p>
+            <div className='inner'>
+              <div className='inner_1'>
+                <p className='bold author'>by {bookDetails.authors}</p>
+                <br />
+                <p><span className="bold">Genre(s):</span> {bookDetails.genres}</p>
+                <p><span className="bold">ISBN:</span> {bookDetails.isbn}</p>
+                
+                <br />
+                <p>{bookDetails.description}</p>
+                <div className="status-info">{getAvailability(bookDetails, appData.user)}</div>
+              </div>
+              <div className='inner_2'>
+                {bookDetails.status.available && !appData.isLoggedIn && 
+                  <Form>
+                    <Button variant="primary" type="submit" disabled>
+                      Sign In to Request
+                    </Button>
+                  </Form>}
+                {bookDetails.status.available && appData.isLoggedIn && !bookDetails.userHasBook &&
+                  <Form onSubmit={handleSubmit}>
+                    <Button className='button' type="submit">
+                      Get This Book
+                    </Button>
+                  </Form>}
+              </div>
+            </div>
+            {appData.isLoggedIn && !bookDetails.userHasBook && <div>
+              <p className="bold">Book Location:</p>
+              <br />
+              <iframe title="Book Location" style={{width: "100%", height: "300px"}} id="gmap_canvas" src={bookDetails.mapUrl}></iframe>
+            </div>}
+          </div>
+  
         </div>
+  
+      </>
+    );
+  }
 
-      </div>
-
-    </>
-  );
 }
 
 export default Book;
