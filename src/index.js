e1428c07e5ff482b4c2bf2f9058a5395f9092b74
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-25 08:06:20 -0400

    add netlify config file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Application from './Application';
+import axios from 'axios';
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
